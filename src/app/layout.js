49da91c37ca169434cba486d6441d6d0e301d4b3
jeeheezy <jeeho032399@gmail.com>
2024-09-23 20:11:28 -0500

    feat: add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
+  console.log("test")
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {timestamp}</footer>
       </body>
     </html>
   );
